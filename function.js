64fed52fe71bfcdc6f53e05747b6dd401b5dcbbb
Dor3cas <niyigenadorcas6@gmail.com>
2025-11-29 12:49:11 +0200

    functions

---
+++ function.js
@@ -0,0 +1,5 @@
+function subtract(number1,number2){
+    return number1-number2
+}
+let difference = subtract(10,5)
+console.log ("The difference of 10 and 5 is "+ difference)
---
+++ study.js
@@ -0,0 +1,16 @@
+/*let spring="Hello";
+let name="guest";
+let newSpring=`${spring} ${name}`;
+console.log(newSpring);*/
+let friends=["jane" ,"mary" , "jessy", ];
+console.log (friends);
+console.log(friends[0]);
+friends.push("joyce");
+console.log(friends);
+console.log(friends[1]);
+/*let text = "Hello, World!";
+console.log(text.length);*/
+
+
+
+
